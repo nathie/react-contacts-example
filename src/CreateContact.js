657077e6778cdb4d4305d2ec2380eef1bf51e00a
Gleidy Mendoza <gleidymendoza@gmail.com>
2017-08-29 14:05:45 -0700

    feat: Form Serialize Serialize the form values into an object to create a new contact.

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
+
+  // Handles the form submit
+  handleSubmit = (e) => {
+    e.preventDefault()
+    // Does the same as the browser does but
+    // in this case creates an object instead
+    const values = serializeForm(e.target, { hash: true })
+
+    // Now we pass the form values to the onCreateContact method
+    if (this.props.onCreateContact)
+        this.props.onCreateContact(values)
+  }
   render() {
     return (
       <div>
         <Link className='close-create-contact' to='/'>Close</Link>
-        <form className='create-contact-form'>
+        <form onSubmit={this.handleSubmit} className='create-contact-form'>
           <ImageInput
             className='create-contact-avatar-input'
             name='avatarURL'
